c3ba58704271174f79e4817ae316d43db070a144
Peter Rohde <dr.rohde@gmail.com>
2022-04-25 08:38:48 +1000

    added menstrual mood overlay

--- MoodSnap/InsightsView/MenstrualView.swift
+++ MoodSnap/InsightsView/MenstrualView.swift
@@ -14,13 +14,30 @@ struct MenstrualView: View {
         // let entries = makeBarData(y: weightData, timescale: timescale)
         let entries2 = makeBarData2(y: weightData, timescale: timescale)
         
+        let entriesE2 = makeBarData2(y: data.processedData.levelE, timescale: timescale)
+        let entriesD2 = makeBarData2(y: data.processedData.levelD, timescale: timescale)
+        let entriesA2 = makeBarData2(y: data.processedData.levelA, timescale: timescale)
+        let entriesI2 = makeBarData2(y: data.processedData.levelI, timescale: timescale)
+
+       // let entries = [entriesE, entriesD, entriesA, entriesI]
+        let color = moodUIColors(settings: data.settings)
+
         if samples == 0 {//|| r2mood[0] == nil || r2mood[1] == nil || r2mood[2] == nil || r2mood[3] == nil {
             Text("insufficient_data")
                 .font(.caption)
                 .foregroundColor(.secondary)
         } else {
-            VerticalBarChart2(values: entries2, color: themes[data.settings.theme].buttonColor, min: 0, max: 1, settings: data.settings)
-                .frame(height: 60)
+            ZStack {
+                VerticalBarChart2(values: entries2,
+                                  color: themes[data.settings.theme].buttonColor,
+                                  min: 0,
+                                  max: 1,
+                                  settings: data.settings)
+                    .frame(height: 60)
+                BorderlessLineChart2(data: [entriesE2, entriesD2, entriesA2, entriesI2],
+                                     color: [Color(color[0]), Color(color[1]), Color(color[2]), Color(color[3])])
+                    .frame(height: 60)
+            }
         }
     }
 }
