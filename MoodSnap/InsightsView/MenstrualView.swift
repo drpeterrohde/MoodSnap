173ee54c3f02913d25fb7d786a0b6eb9f457bf2e
Peter Rohde <dr.rohde@gmail.com>
2022-04-26 10:48:40 +1000

    V2 alpha

--- MoodSnap/InsightsView/MenstrualView.swift
+++ MoodSnap/InsightsView/MenstrualView.swift
@@ -31,17 +31,6 @@ struct MenstrualView: View {
                 .foregroundColor(.secondary)
         } else {
             ZStack {
-//                VerticalBarChartOpacity2(values: entries,
-//                                         color: themes[data.settings.theme].buttonColor,
-//                                         min: 0,
-//                                         max: 1,
-//                                         shaded: true,
-//                                         settings: data.settings)
-//                    .frame(height: 60)
-//                BorderlessLineChart2(data: moodEntries,
-//                                     color: [Color(color[0]), Color(color[1]), Color(color[2]), Color(color[3])])
-//                    .frame(height: 60)
-                
                 SuperimposedCharLineChart(barData: entries,
                                          lineData: moodEntries,
                                          barColor: themes[data.settings.theme].buttonColor,
@@ -49,7 +38,7 @@ struct MenstrualView: View {
                                          shaded: true,
                                          settings: data.settings)
             }
-            LineChart2(data: butterflyData, color: [Color(color[0]), Color(color[1]), Color(color[2]), Color(color[3])], min: -bound, max: bound, horizontalGridLines: 1, verticalGridLines: 1) // ???
+           // LineChart2(data: butterflyData, color: [Color(color[0]), Color(color[1]), Color(color[2]), Color(color[3])], min: -bound, max: bound, horizontalGridLines: 1, verticalGridLines: 1) // ???
         }
     }
 }
