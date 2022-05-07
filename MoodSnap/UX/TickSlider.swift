5fbc80a04f82bb2e96a6b421419c2fc3c2075aa7
Peter Rohde <dr.rohde@gmail.com>
2022-05-07 12:18:25 +1000

    disabled slider tick marks

--- MoodSnap/UX/TickSlider.swift
+++ MoodSnap/UX/TickSlider.swift
@@ -1,34 +1,34 @@
-import SwiftUI
-
-struct TickSlider: View {
-    @Binding var value: CGFloat
-
-    var body: some View {
-        VStack {
-            ZStack {
-                    HStack {
-                        Text("    I")
-                            .foregroundColor(.gray.opacity(0.25))
-                            .font(.system(size: 9))
-                        Spacer()
-                        Text("I")
-                            .foregroundColor(.gray.opacity(0.25))
-                            .font(.system(size: 9))
-                        Spacer()
-                        Text("I")
-                            .foregroundColor(.gray.opacity(0.25))
-                            .font(.system(size: 9))
-                        Spacer()
-                        Text("I")
-                            .foregroundColor(.gray.opacity(0.25))
-                            .font(.system(size: 9))
-                        Spacer()
-                        Text("I    ")
-                            .foregroundColor(.gray.opacity(0.25))
-                            .font(.system(size: 9))
-                    }
-                    Slider(value: $value, in: 0...4, step: 1)
-            }
-        }
-    }
-}
+//import SwiftUI
+//
+//struct TickSlider: View {
+//    @Binding var value: CGFloat
+//
+//    var body: some View {
+//        VStack {
+//            ZStack {
+//                    HStack {
+//                        Text("    I")
+//                            .foregroundColor(.gray.opacity(0.25))
+//                            .font(.system(size: 9))
+//                        Spacer()
+//                        Text("I")
+//                            .foregroundColor(.gray.opacity(0.25))
+//                            .font(.system(size: 9))
+//                        Spacer()
+//                        Text("I")
+//                            .foregroundColor(.gray.opacity(0.25))
+//                            .font(.system(size: 9))
+//                        Spacer()
+//                        Text("I")
+//                            .foregroundColor(.gray.opacity(0.25))
+//                            .font(.system(size: 9))
+//                        Spacer()
+//                        Text("I    ")
+//                            .foregroundColor(.gray.opacity(0.25))
+//                            .font(.system(size: 9))
+//                    }
+//                    Slider(value: $value, in: 0...4, step: 1)
+//            }
+//        }
+//    }
+//}
