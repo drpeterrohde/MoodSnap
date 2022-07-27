39953430a8cf592b6c436bfc0a97e1754253d550
Peter Rohde <dr.rohde@gmail.com>
2022-07-27 17:39:34 +0200

    more async await

--- MoodSnap/DataStore/DataStore.swift
+++ MoodSnap/DataStore/DataStore.swift
@@ -34,10 +34,7 @@ struct DataStoreStruct: Identifiable, Codable, Hashable {
         }
     }
 
-    /**
-     Pre-process data.
-     */
-    mutating func process() async {
+    mutating func processHistory() async -> Bool {
         let history = await generateHistory(data: self)
         
         // Mood history
@@ -57,10 +54,11 @@ struct DataStoreStruct: Identifiable, Codable, Hashable {
         self.processedData.volatilityD = history.volatilityD
         self.processedData.volatilityA = history.volatilityA
         self.processedData.volatilityI = history.volatilityI
-
-        // Butterflies
-
-        // Event
+        
+        return true
+    }
+    
+    mutating func processEvents() async -> Bool {
         let eventList = getEventsList(moodSnaps: self.moodSnaps)
         var eventButterflies: [ButterflyEntryStruct] = []
         for i in 0 ..< eventList.count {
@@ -74,24 +72,11 @@ struct DataStoreStruct: Identifiable, Codable, Hashable {
             eventButterflies.append(thisButterfly)
         }
         self.processedData.eventButterfly = eventButterflies
-
-        // Hashtags
-        let hashtags = getHashtags(data: self)
-        var hashtagButterflies: [ButterflyEntryStruct] = []
-        for i in 0 ..< hashtags.count {
-            let dates = getDatesForHashtag(
-                hashtag: hashtags[i],
-                moodSnaps: self.moodSnaps)
-            var thisButterfly = averageTransientForDates(
-                dates: dates,
-                moodSnaps: self.moodSnaps,
-                maxWindow: butterflyWindowShort)
-            thisButterfly.activity = hashtags[i]
-            hashtagButterflies.append(thisButterfly)
-        }
-        self.processedData.hashtagButterfly = hashtagButterflies
-
-        // Activity
+        
+        return true
+    }
+    
+    mutating func processActivities() async -> Bool {
         var activityButterflies: [ButterflyEntryStruct] = []
         for i in 0 ..< activityList.count {
             let dates = getDatesForType(
@@ -106,24 +91,11 @@ struct DataStoreStruct: Identifiable, Codable, Hashable {
             activityButterflies.append(thisButterfly)
         }
         self.processedData.activityButterfly = activityButterflies
-
-        // Social
-        var socialButterflies: [ButterflyEntryStruct] = []
-        for i in 0 ..< socialList.count {
-            let dates = getDatesForType(
-                type: .social,
-                item: i,
-                moodSnaps: self.moodSnaps)
-            var thisButterfly = averageTransientForDates(
-                dates: dates,
-                moodSnaps: self.moodSnaps,
-                maxWindow: butterflyWindowShort)
-            thisButterfly.activity = socialList[i]
-            socialButterflies.append(thisButterfly)
-        }
-        self.processedData.socialButterfly = socialButterflies
-
-        // Symptom
+        
+        return true
+    }
+    
+    mutating func processSymptoms() async -> Bool {
         var symptomButterflies: [ButterflyEntryStruct] = []
         for i in 0 ..< symptomList.count {
             let dates = getDatesForType(
@@ -138,6 +110,60 @@ struct DataStoreStruct: Identifiable, Codable, Hashable {
             symptomButterflies.append(thisButterfly)
         }
         self.processedData.symptomButterfly = symptomButterflies
+        
+        return true
+    }
+    
+    mutating func processSocial() async -> Bool {
+        var socialButterflies: [ButterflyEntryStruct] = []
+        for i in 0 ..< socialList.count {
+            let dates = getDatesForType(
+                type: .social,
+                item: i,
+                moodSnaps: self.moodSnaps)
+            var thisButterfly = averageTransientForDates(
+                dates: dates,
+                moodSnaps: self.moodSnaps,
+                maxWindow: butterflyWindowShort)
+            thisButterfly.activity = socialList[i]
+            socialButterflies.append(thisButterfly)
+        }
+        self.processedData.socialButterfly = socialButterflies
+        
+        return true
+    }
+    
+    mutating func processHashtags() async -> Bool {
+        let hashtags = getHashtags(data: self)
+        var hashtagButterflies: [ButterflyEntryStruct] = []
+        for i in 0 ..< hashtags.count {
+            let dates = getDatesForHashtag(
+                hashtag: hashtags[i],
+                moodSnaps: self.moodSnaps)
+            var thisButterfly = averageTransientForDates(
+                dates: dates,
+                moodSnaps: self.moodSnaps,
+                maxWindow: butterflyWindowShort)
+            thisButterfly.activity = hashtags[i]
+            hashtagButterflies.append(thisButterfly)
+        }
+        self.processedData.hashtagButterfly = hashtagButterflies
+        
+        return true
+    }
+    
+    /**
+     Pre-process data.
+     */
+    mutating func process() async {
+        let dummy1 = await self.processHistory()
+        let dummy2 = await self.processEvents()
+        let dummy3 = await self.processActivities()
+        let dummy4 = await self.processSymptoms()
+        let dummy5 = await self.processSocial()
+        let dummy6 = await self.processHashtags()
+        
+        //await [dummy1, dummy2, dummy3, dummy4, dummy5, dummy6]
     }
 
     /**
