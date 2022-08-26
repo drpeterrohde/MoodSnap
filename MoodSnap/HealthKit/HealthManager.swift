a469b7968c6d6d625047584dd465a7db45c74d0d
Peter Rohde <dr.rohde@gmail.com>
2022-08-26 19:27:42 +0200

    cleanup healthkit processes

--- MoodSnap/HealthKit/HealthManager.swift
+++ MoodSnap/HealthKit/HealthManager.swift
@@ -43,176 +43,164 @@ final class HealthManager: ObservableObject {
     @Published var menstrualButterfly: ButterflyEntryStruct = ButterflyEntryStruct()
     
     public let healthStore = HKHealthStore()
-
+    
     public func requestPermissions() {
         let readDataTypes: Set = [HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!,
                                   HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!,
                                   HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!,
                                   HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.menstrualFlow)!,
                                   HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!]
-
+        
         healthStore.requestAuthorization(toShare: nil, read: readDataTypes, completion: { success, error in })
     }
-
+    
     /**
      Generate `HealthSnapStruct`s for all dates and start processing.
      */
     func makeHealthSnaps(data: DataStoreClass) async {
         var date: Date = getLastDate(moodSnaps: data.moodSnaps)
         let earliest: Date = getFirstDate(moodSnaps: data.moodSnaps)
-
+        
         self.stopProcessing()
         
         self.healthSnaps = []
-
+        
         let group = DispatchGroup()
-
+        
         while date >= earliest {
             await makeHealthSnapForDate(date: date, group: group)
             date = date.addDays(days: -1)
         }
-
+        
         group.notify(queue: DispatchQueue.global()) {
-            //DispatchQueue.main.async {
-                data.healthSnaps = self.healthSnaps
-            //}
+            data.healthSnaps = self.healthSnaps
             self.startProcessing(data: data)
         }
     }
-
+    
     /**
      Generate `HealthSnapStruct`s for a given date.
      */
     @inline(__always) func makeHealthSnapForDate(date: Date, group: DispatchGroup) async {
         let startDate = date.startOfDay()
         let endDate = date.endOfDay()
-
+        
         let quantityTypeWeight = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.bodyMass)!
         let quantityTypeDistance = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.distanceWalkingRunning)!
         let quantityTypeActiveEnergy = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned)!
         let quantityTypeMenstrual = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.menstrualFlow)!
         let quantityTypeSleep = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
-
+        
         let queue = DispatchQueue(label: "thread-safe-array")
         
         let predicate = HKQuery.predicateForSamples(withStart: startDate,
                                                     end: endDate,
                                                     options: .strictStartDate)
-
+        
         group.enter()
         let sampleQueryWeight = HKSampleQuery(sampleType: quantityTypeWeight,
                                               predicate: predicate,
                                               limit: HKObjectQueryNoLimit,
                                               sortDescriptors: nil,
                                               resultsHandler: { _, results, _ in
-            DispatchQueue.global(qos: .userInteractive).async {
-                let maxWeight = self.maxWeight(results: results)
-                if maxWeight != nil {
-                    var healthSnap = HealthSnapStruct()
-                    healthSnap.timestamp = date
-                    healthSnap.weight = CGFloat(maxWeight!)
-                    let healthSnapUI = healthSnap
-                    queue.async() {
-                        self.healthSnaps.append(healthSnapUI)
-                    }
+            let maxWeight = self.maxWeight(results: results)
+            if maxWeight != nil {
+                var healthSnap = HealthSnapStruct()
+                healthSnap.timestamp = date
+                healthSnap.weight = CGFloat(maxWeight!)
+                let healthSnapUI = healthSnap
+                queue.async() {
+                    self.healthSnaps.append(healthSnapUI)
                 }
-                group.leave()
             }
+            group.leave()
         })
-
+        
         group.enter()
         let sampleQueryDistance = HKSampleQuery(sampleType: quantityTypeDistance,
                                                 predicate: predicate,
                                                 limit: HKObjectQueryNoLimit,
                                                 sortDescriptors: nil,
                                                 resultsHandler: { _, results, _ in
-            DispatchQueue.global(qos: .userInteractive).async {
-                let distance = self.totalDistance(results: results)
-                if distance != nil {
-                    var healthSnap = HealthSnapStruct()
-                    healthSnap.timestamp = date
-                    healthSnap.walkingRunningDistance = CGFloat(distance!)
-                    let healthSnapUI = healthSnap
-                    queue.async() {
-                        self.healthSnaps.append(healthSnapUI)
-                    }
+            let distance = self.totalDistance(results: results)
+            if distance != nil {
+                var healthSnap = HealthSnapStruct()
+                healthSnap.timestamp = date
+                healthSnap.walkingRunningDistance = CGFloat(distance!)
+                let healthSnapUI = healthSnap
+                queue.async() {
+                    self.healthSnaps.append(healthSnapUI)
                 }
-                group.leave()
             }
+            group.leave()
         })
-
+        
         group.enter()
         let sampleQueryActiveEnergy = HKSampleQuery(sampleType: quantityTypeActiveEnergy,
                                                     predicate: predicate,
                                                     limit: HKObjectQueryNoLimit,
                                                     sortDescriptors: nil,
                                                     resultsHandler: { _, results, _ in
-            DispatchQueue.global(qos: .userInteractive).async {
-                let energy = self.totalEnergy(results: results)
-                if energy != nil {
-                    var healthSnap = HealthSnapStruct()
-                    healthSnap.timestamp = date
-                    healthSnap.activeEnergy = CGFloat(energy!)
-                    let healthSnapUI = healthSnap
-                    queue.async() {
-                        self.healthSnaps.append(healthSnapUI)
-                    }
+            let energy = self.totalEnergy(results: results)
+            if energy != nil {
+                var healthSnap = HealthSnapStruct()
+                healthSnap.timestamp = date
+                healthSnap.activeEnergy = CGFloat(energy!)
+                let healthSnapUI = healthSnap
+                queue.async() {
+                    self.healthSnaps.append(healthSnapUI)
                 }
-                group.leave()
             }
+            group.leave()
         })
-
+        
         group.enter()
         let sampleQueryMenstrual = HKSampleQuery(sampleType: quantityTypeMenstrual,
                                                  predicate: predicate,
                                                  limit: HKObjectQueryNoLimit,
                                                  sortDescriptors: nil,
                                                  resultsHandler: { _, results, _ in
-            DispatchQueue.global(qos: .userInteractive).async {
-                let menstrual = self.maxMenstrual(results: results)
-                if menstrual != nil {
-                    var healthSnap = HealthSnapStruct()
-                    healthSnap.timestamp = date
-                    healthSnap.menstrual = CGFloat(menstrual!)
-                    if healthSnap.menstrual != 0 {
-                        let healthSnapUI = healthSnap
-                        queue.async() {
-                            self.healthSnaps.append(healthSnapUI)
-                        }
+            let menstrual = self.maxMenstrual(results: results)
+            if menstrual != nil {
+                var healthSnap = HealthSnapStruct()
+                healthSnap.timestamp = date
+                healthSnap.menstrual = CGFloat(menstrual!)
+                if healthSnap.menstrual != 0 {
+                    let healthSnapUI = healthSnap
+                    queue.async() {
+                        self.healthSnaps.append(healthSnapUI)
                     }
                 }
-                group.leave()
             }
+            group.leave()
         })
-
+        
         group.enter()
         let sampleQuerySleep = HKSampleQuery(sampleType: quantityTypeSleep,
                                              predicate: predicate,
                                              limit: HKObjectQueryNoLimit,
                                              sortDescriptors: nil,
                                              resultsHandler: { _, results, _ in
-            DispatchQueue.global(qos: .userInteractive).async {
-                let sleep = self.totalSleep(results: results)
-                if sleep != nil {
-                    var healthSnap = HealthSnapStruct()
-                    healthSnap.timestamp = date
-                    healthSnap.sleepHours = CGFloat(sleep!)
-                    let healthSnapUI = healthSnap
-                    queue.async() {
-                        self.healthSnaps.append(healthSnapUI)
-                    }
+            let sleep = self.totalSleep(results: results)
+            if sleep != nil {
+                var healthSnap = HealthSnapStruct()
+                healthSnap.timestamp = date
+                healthSnap.sleepHours = CGFloat(sleep!)
+                let healthSnapUI = healthSnap
+                queue.async() {
+                    self.healthSnaps.append(healthSnapUI)
                 }
-                group.leave()
             }
+            group.leave()
         })
-
+        
         healthStore.execute(sampleQueryWeight)
         healthStore.execute(sampleQueryDistance)
         healthStore.execute(sampleQueryActiveEnergy)
         healthStore.execute(sampleQueryMenstrual)
         healthStore.execute(sampleQuerySleep)
     }
-
+    
     /**
      Maximum weight for given HealthKit `results`
      */
@@ -220,22 +208,22 @@ final class HealthManager: ObservableObject {
         if results == nil {
             return nil
         }
-
+        
         if results!.count == 0 {
             return nil
         }
-
+        
         var maxKg: Double = 0
-
+        
         for result in results! {
             let thisMassSample = result as! HKQuantitySample
             let thisMassKg = thisMassSample.quantity.doubleValue(for: HKUnit.gramUnit(with: .kilo))
             maxKg = max(maxKg, thisMassKg)
         }
-
+        
         return maxKg
     }
-
+    
     /**
      Total sleep for given HealthKit `results`
      */
@@ -243,13 +231,13 @@ final class HealthManager: ObservableObject {
         if results == nil {
             return nil
         }
-
+        
         if results!.count == 0 {
             return nil
         }
-
+        
         var sleep: Double = 0.0
-
+        
         for result in results! {
             let thisSleepSample = result as! HKCategorySample
             if thisSleepSample.value == HKCategoryValueSleepAnalysis.inBed.rawValue {
@@ -259,10 +247,10 @@ final class HealthManager: ObservableObject {
                 sleep += Double(timeInterval)
             }
         }
-
+        
         return sleep
     }
-
+    
     /**
      Total distance for given HealthKit `results`
      */
@@ -270,21 +258,21 @@ final class HealthManager: ObservableObject {
         if results == nil {
             return nil
         }
-
+        
         if results!.count == 0 {
             return nil
         }
-
+        
         var distance: Double = 0.0
-
+        
         for result in results! {
             let thisDistanceSample = result as! HKQuantitySample
             distance += thisDistanceSample.quantity.doubleValue(for: HKUnit.meterUnit(with: .kilo))
         }
-
+        
         return distance
     }
-
+    
     /**
      Total active energy for given HealthKit `results`
      */
@@ -292,21 +280,21 @@ final class HealthManager: ObservableObject {
         if results == nil {
             return nil
         }
-
+        
         if results!.count == 0 {
             return nil
         }
-
+        
         var energy: Double = 0.0
-
+        
         for result in results! {
             let thisEnergySample = result as! HKQuantitySample
             energy += thisEnergySample.quantity.doubleValue(for: HKUnit.jouleUnit(with: .kilo))
         }
-
+        
         return energy
     }
-
+    
     /**
      Maximum menstrual flow for given HealthKit `results`
      */
@@ -314,20 +302,20 @@ final class HealthManager: ObservableObject {
         if results == nil {
             return nil
         }
-
+        
         var flow = 0
-
+        
         for result in results! {
             let thisMenstrualSample = result as! HKCategorySample
-
+            
             if Int(thisMenstrualSample.value) != HKCategoryValueMenstrualFlow.none.rawValue {
                 flow = thisMenstrualSample.value
             }
         }
-
+        
         return Double(flow)
     }
-
+    
     /**
      Process weight data.
      */
@@ -356,7 +344,7 @@ final class HealthManager: ObservableObject {
         
         return true
     }
-        
+    
     /**
      Process sleep data
      */
@@ -367,7 +355,7 @@ final class HealthManager: ObservableObject {
         let sleepCorrelationsMoodUI: [CGFloat?] = getCorrelation(data: data, health: self, type: .sleep)
         let sleepDataUI: [CGFloat?] = getSleepData(data: data, health: self)
         let maxSleepUI: CGFloat = maxWithNils(data: sleepDataUI) ?? 0
-
+        
         DispatchQueue.main.async {
             self.sleepSamples = sleepSamplesUI
             self.sleepAverage = sleepAverageUI
@@ -446,7 +434,7 @@ final class HealthManager: ObservableObject {
         
         return true
     }
-        
+    
     /**
      Pre-process data.
      */
@@ -459,7 +447,7 @@ final class HealthManager: ObservableObject {
         
         await _ = [weightComplete, sleepComplete, energyComplete, distanceComplete, processMenstrual]
     }
-
+    
     /**
      Start asynchronous processing of data
      */
